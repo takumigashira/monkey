93441008938e81c472ec64b66e1dc5148c740b2c
takumigashira <takumigashira@takumigashiranoMacBook-puro.local>
2018-06-27 21:39:53 +0900

    add lexer_test

--- token/token.go
+++ token/token.go
@@ -3,8 +3,33 @@ package token
 
 type TokenType string
 
-type Token struct {
-  Type   TokenType
-  Literal string
-}
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
 
+	//識別子
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	//演算子
+	ASSIGN = "="
+	PLUS   = "+"
+
+	//デリミタ
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	//キーワード
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
